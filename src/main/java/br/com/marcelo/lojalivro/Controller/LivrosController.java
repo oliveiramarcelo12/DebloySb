231e7d8df89826c795dba789ae0ec1f716937e5f
oliveiramarcelo12 <oliveiramarcelo198@gmail.com>
2024-05-08 21:44:18 -0300

    08/05

--- src/main/java/br/com/marcelo/lojalivro/Controller/LivrosController.java
+++ src/main/java/br/com/marcelo/lojalivro/Controller/LivrosController.java
@@ -1,33 +1,38 @@
 package br.com.marcelo.lojalivro.Controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import br.com.marcelo.lojalivro.Model.Livro;
 import br.com.marcelo.lojalivro.Repository.LivroRepository;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LivrosController {
     @Autowired
     LivroRepository livroRepository;
+
     @GetMapping("livros")
-    public List<Livro> getLivros(@RequestParam String param) {
-        
-        return new String();
+    public ModelAndView getLivros() {
+        ModelAndView mv = new ModelAndView("livros.list");
+        mv.addObject("livros", livroRepository.findAll());
+        return mv;
+
     }
-    @PostMapping("livros")
-    public String postLivro(Livro livro ) {
+
+    @GetMapping("addlivro")
+    public String getAddLivro() {
+        return "livro-add";
+    }
+
+    @PostMapping("/addlivro")
+    public String postAddLivro(Livro livro) {
         livroRepository.save(livro);
-        
-        return "/livro";
+
+        return "redirect:/livro";
     }
-    
 
 }
